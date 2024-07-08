b466af7c7b3c6cc7bf08896240c63a725523e4b2
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-07-08 18:38:54 +0200

    Added start-screen and beginning of the game screen

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Game } from '../../models/game';
+
+@Component({
+  selector: 'app-game',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.scss'
+})
+export class GameComponent implements OnInit{
+
+  pickCardAnimation = false;
+  game: Game | undefined;
+
+  constructor() {
+  }
+
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+
+  takeCard() {
+    this.pickCardAnimation = true;
+  }
+}
